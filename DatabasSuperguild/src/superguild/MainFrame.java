be9df0417a0a6344311fb72c229fd4912b772ba7
Joakim Lavenius <theguidingblue@gmail.com>
2013-03-19 13:22:40 +0100

    Små ändringar. Testar.

--- DatabasSuperguild/src/superguild/MainFrame.java
+++ DatabasSuperguild/src/superguild/MainFrame.java
@@ -1,5 +1,17 @@
 package superguild;
 
-public class MainFrame {
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
+public class MainFrame extends JFrame {
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(new Dimension(500, 400));
+		setTitle("Superguild");
+		setName("MainJFrame");
+		setMinimumSize(new Dimension(500, 400));
+	}
+
+	private static final long serialVersionUID = 1L;
 
 }
