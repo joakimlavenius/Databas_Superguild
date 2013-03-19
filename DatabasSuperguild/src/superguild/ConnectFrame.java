c3d4cf8e79c1d96c617204abc10c0f2d220d10dc
Mattias Hagelin <mattias1985@gmail.com>
2013-03-19 14:04:47 +0100

    woho

--- DatabasSuperguild/src/superguild/ConnectFrame.java
+++ DatabasSuperguild/src/superguild/ConnectFrame.java
@@ -1,5 +1,131 @@
 package superguild;
 
-public class ConnectFrame {
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+public class ConnectFrame extends JDialog{
+	private JPasswordField passwordField;
+	private JTextField textField;
+	private MysqlDataSource ds = null;
+	private Connection con = null;
+	private JTextField textField_1;
+	public ConnectFrame() {
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.NORTH);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{100, 100, 100};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, 1.0, 0.0};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblNewLabel = new JLabel("Username");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.insets = new Insets(10, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		panel.add(lblNewLabel, gbc_lblNewLabel);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(10, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		panel.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblH = new JLabel("Host");
+		GridBagConstraints gbc_lblH = new GridBagConstraints();
+		gbc_lblH.anchor = GridBagConstraints.NORTHEAST;
+		gbc_lblH.insets = new Insets(0, 0, 5, 5);
+		gbc_lblH.gridx = 0;
+		gbc_lblH.gridy = 1;
+		panel.add(lblH, gbc_lblH);
+		
+		textField_1 = new JTextField("localhost");
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 1;
+		panel.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Password");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 3;
+		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		passwordField = new JPasswordField();
+		GridBagConstraints gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.insets = new Insets(0, 0, 5, 5);
+		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordField.gridx = 1;
+		gbc_passwordField.gridy = 3;
+		panel.add(passwordField, gbc_passwordField);
+		
+		JButton btnSignIn = new JButton("Sign In");
+		GridBagConstraints gbc_btnSignIn = new GridBagConstraints();
+		gbc_btnSignIn.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSignIn.gridwidth = 1;
+		gbc_btnSignIn.gridx = 1;
+		gbc_btnSignIn.gridy = 4;
+		
+		btnSignIn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ds = new MysqlDataSource();
+				
+				ds.setServerName(textField_1.getText());
+				ds.setPort(3306);
+
+				ds.setUser(textField.getText());
+				ds.setPassword(passwordField.getText());
+				ds.setDatabaseName("superguild");
+				setVisible(false);
+				
+				try {
+					con = ds.getConnection();
+				} catch (SQLException e1) {
+					System.err.println("Connection failed " + e1.getMessage());
+					return;
+				}
+				
+				System.out.println("Connection sucessful");
+			}
+		});
+		
+		panel.add(btnSignIn, gbc_btnSignIn);
+		
+		setTitle("Connect");
+		pack();
+		setResizable(false);
+		setModal(true);
+		setLocationRelativeTo(null);
+		setVisible(true);
+
+	}
 
 }
